809648fd8b0046b0a82e084a6aff839c7d03956b
abhinav khanna <kabhinav99@gmail.com>
2024-11-30 03:00:45 +0530

    fix : fixed alignment in first and last section

--- src/app/featured/page.jsx
+++ src/app/featured/page.jsx
@@ -16,19 +16,19 @@ const FeaturedPage = () => {
       <div
         className=" featured-bg flex flex-col items-center justify-center bg-cover bg-center"
       >
-        <div className="flex flex-col justify-center items-center h-full text-center gap-6 px-4">
-          <h2 className="heading-font font-bold text-4xl text-white drop-shadow-lg mb-6 ">
+        <div className="flex flex-col justify-center items-center h-full text-center gap-6  px-4">
+          <h2 className="heading-font font-bold text-3xl text-white drop-shadow-lg py-20 leading-relaxed ">
             FEATURED 10X10 TRADE SHOW BOOTH RENTALS
           </h2>
           <div className="flex flex-col items-center gap-4">
-            <div className="font-bold text-white text-2xl drop-shadow-sm">
+            <div className="font-bold text-white text-2xl drop-shadow-sm leading-relaxed ">
               TUIOW202039002
             </div>
             <Button
               style={{ transitionDuration: "500ms" }}
-              className="bg-transparent hover:bg-[#B0CB1F] border-2 border-[#B0CB1F] text-[#B0CB1F] hover:text-secondary text-lg font-semibold px-6 py-2 "
+              className="bg-transparent hover:bg-[#B0CB1F] border-2 border-[#B0CB1F] text-[#B0CB1F] hover:text-secondary text-lg font-semibold px-[10px] py-6 "
             >
-              Customize Now!
+              Request for Free design 
             </Button>
           </div>
         </div>
