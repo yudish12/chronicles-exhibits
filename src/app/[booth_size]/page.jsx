1be9fb27dede0287a1ad5ac8ba088e3cd51adfc5
yudish12 <yudish.chakrawarty@mindcase.co>
2024-11-30 15:48:00 +0530

    page refactor

--- src/app/[booth_size]/page.jsx
+++ src/app/[booth_size]/page.jsx
@@ -8,17 +8,19 @@ import BoothGrid from "./_components/AllBooths";
 import RequestDesign from "./_components/RequestDesign";
 import TradeShowSection from "./_components/TradeShowSection";
 
-const FeaturedPage = ({ params }) => {
+const FeaturedPage = async ({ params }) => {
+  const boothSizes = await params.booth_size;
+
   return (
     <>
       <SubHeader />
       <Header />
       <div className=" featured-bg flex flex-col items-center justify-center bg-cover bg-center">
         <div className="flex flex-col justify-center items-center h-full text-center gap-6  px-4">
-          <h2 className="heading-font font-bold text-3xl text-white drop-shadow-lg py-20 leading-relaxed ">
-            FEATURED {params.booth_size} TRADE SHOW BOOTH RENTALS
+          <h2 className="heading-font font-bold text-[2rem] text-white drop-shadow-lg leading-relaxed ">
+            FEATURED {boothSizes} TRADE SHOW BOOTH RENTALS
           </h2>
-          <div className="flex flex-col items-center gap-4">
+          <div className="flex flex-col items-center justify-self-end gap-4">
             <div className="font-bold text-white text-2xl drop-shadow-sm leading-relaxed ">
               TUIOW202039002
             </div>
@@ -31,9 +33,9 @@ const FeaturedPage = ({ params }) => {
           </div>
         </div>
       </div>
-      <BoothGrid size={params.booth_size} />
-      <RequestDesign size={params.booth_size} />
-      <TradeShowSection size={params.booth_size} />
+      <BoothGrid size={boothSizes} />
+      <RequestDesign size={boothSizes} />
+      <TradeShowSection size={boothSizes} />
       <Queryform />
       <Footer />
     </>
