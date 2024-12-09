7cdba2e8fc408863c319ffbbf66dc8b7c2085e99
yudish12 <yudish.chakrawarty@mindcase.co>
2024-12-09 22:47:30 +0530

    apos

--- src/app/about-us/_components/Introduction.jsx
+++ src/app/about-us/_components/Introduction.jsx
@@ -3,34 +3,34 @@ import React from 'react'
 const Introduction = () => {
   return (
     <div className='flex flex-col items-center justify-center  py-12 w-full '>
-        <h2 className='text-secondary text-4xl heading-font py-6 '>A LEADING TRADE SHOW BOOTH DESIGN COMPANY IN USA </h2>
-        <div className='py-2 text-sm '>We've been creating unique rental exhibits to promote your brand for more than Two decades !</div>
-        <div className='text-secondary heading-font mt-10 mb-6 text-3xl  '><h2>WHO WE ARE </h2></div>
-        <div className='text-secondary text-center px-12 '>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      <h2 className='text-secondary text-4xl heading-font py-6 '>A LEADING TRADE SHOW BOOTH DESIGN COMPANY IN USA </h2>
+      <div className='py-2 text-sm '>We&apos;ve been creating unique rental exhibits to promote your brand for more than Two decades !</div>
+      <div className='text-secondary heading-font mt-10 mb-6 text-3xl  '><h2>WHO WE ARE </h2></div>
+      <div className='text-secondary text-center px-12 '>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
           Modi unde ea vel voluptatem maiores cumque sint aperiam.
-         Porro ut omnis iure praesentium nesciunt est aut voluptatem quae. 
-         Reiciendis quos repellendus vel voluptatibus quia nesciunt iusto impedit at suscipit molestiae sed veniam, quibusdam officia aut ratione aliquam corrupti illo ad accusantium a fuga reprehenderit sint repellat voluptatem! Inventore exercitationem facere alias at. 
-         Dignissimos molestiae blanditiis officia repellendus dolor. 
-         Ea, ratione sequi officiis harum aspernatur qui iste maxime adipisci dicta minus deleniti molestiae in, perferendis praesentium, corrupti ipsum omnis? Reiciendis, nam, officiis expedita voluptates voluptatibus tempora quibusdam, minima modi sit reprehenderit distinctio? Beatae nam quisquam architecto tempora at odit magnam, nulla sed consequatur dicta, ut laudantium ipsum ex, molestiae ratione quas doloremque quibusdam eveniet.
-        Sint ex necessitatibus excepturi.
-        Ipsum, molestias rerum. Nobis esse possimus, labore, adipisci itaque aspernatur quisquam sit, incidunt explicabo tenetur omnis. 
-        Impedit quasi repellendus doloribus? Ullam ab maxime sed?
+          Porro ut omnis iure praesentium nesciunt est aut voluptatem quae.
+          Reiciendis quos repellendus vel voluptatibus quia nesciunt iusto impedit at suscipit molestiae sed veniam, quibusdam officia aut ratione aliquam corrupti illo ad accusantium a fuga reprehenderit sint repellat voluptatem! Inventore exercitationem facere alias at.
+          Dignissimos molestiae blanditiis officia repellendus dolor.
+          Ea, ratione sequi officiis harum aspernatur qui iste maxime adipisci dicta minus deleniti molestiae in, perferendis praesentium, corrupti ipsum omnis? Reiciendis, nam, officiis expedita voluptates voluptatibus tempora quibusdam, minima modi sit reprehenderit distinctio? Beatae nam quisquam architecto tempora at odit magnam, nulla sed consequatur dicta, ut laudantium ipsum ex, molestiae ratione quas doloremque quibusdam eveniet.
+          Sint ex necessitatibus excepturi.
+          Ipsum, molestias rerum. Nobis esse possimus, labore, adipisci itaque aspernatur quisquam sit, incidunt explicabo tenetur omnis.
+          Impedit quasi repellendus doloribus? Ullam ab maxime sed?
         </p>
-        <p className='py-4'>Dignissimos molestiae blanditiis officia repellendus dolor. 
-         Ea, ratione sequi officiis harum aspernatur qui iste maxime adipisci dicta minus deleniti molestiae in, perferendis praesentium, corrupti ipsum omnis? Reiciendis, nam, officiis expedita voluptates voluptatibus tempora quibusdam, minima modi sit reprehenderit distinctio? Beatae nam quisquam architecto tempora at odit magnam, nulla sed consequatur dicta, ut laudantium ipsum ex, molestiae ratione quas doloremque quibusdam eveniet.
-        Sint ex necessitatibus excepturi.
-        Ipsum, molestias rerum. Nobis esse possimus, labore, adipisci itaque aspernatur quisquam sit, incidunt explicabo tenetur omnis. 
-        Impedit quasi repellendus doloribus? Ullam ab maxime sed?</p>
-        </div>
-        <div className='text-secondary heading-font mt-10 mb-6 text-3xl  '><h2>OUR BELIEFS </h2></div>
-        <div className='text-secondary text-center px-12 '>
-        <p className='py-4'>Dignissimos molestiae blanditiis officia repellendus dolor. 
-         Ea, ratione sequi officiis harum aspernatur qui iste maxime adipisci dicta minus deleniti molestiae in, perferendis praesentium, corrupti ipsum omnis? Reiciendis, nam, officiis expedita voluptates voluptatibus tempora quibusdam, minima modi sit reprehenderit distinctio? Beatae nam quisquam architecto tempora at odit magnam, nulla sed consequatur dicta, ut laudantium ipsum ex, molestiae ratione quas doloremque quibusdam eveniet.
-        Sint ex necessitatibus excepturi.
-        Ipsum, molestias rerum. Nobis esse possimus, labore, adipisci itaque aspernatur quisquam sit, incidunt explicabo tenetur omnis. 
-        Impedit quasi repellendus doloribus? Ullam ab maxime sed?</p>
-        </div>
+        <p className='py-4'>Dignissimos molestiae blanditiis officia repellendus dolor.
+          Ea, ratione sequi officiis harum aspernatur qui iste maxime adipisci dicta minus deleniti molestiae in, perferendis praesentium, corrupti ipsum omnis? Reiciendis, nam, officiis expedita voluptates voluptatibus tempora quibusdam, minima modi sit reprehenderit distinctio? Beatae nam quisquam architecto tempora at odit magnam, nulla sed consequatur dicta, ut laudantium ipsum ex, molestiae ratione quas doloremque quibusdam eveniet.
+          Sint ex necessitatibus excepturi.
+          Ipsum, molestias rerum. Nobis esse possimus, labore, adipisci itaque aspernatur quisquam sit, incidunt explicabo tenetur omnis.
+          Impedit quasi repellendus doloribus? Ullam ab maxime sed?</p>
+      </div>
+      <div className='text-secondary heading-font mt-10 mb-6 text-3xl  '><h2>OUR BELIEFS </h2></div>
+      <div className='text-secondary text-center px-12 '>
+        <p className='py-4'>Dignissimos molestiae blanditiis officia repellendus dolor.
+          Ea, ratione sequi officiis harum aspernatur qui iste maxime adipisci dicta minus deleniti molestiae in, perferendis praesentium, corrupti ipsum omnis? Reiciendis, nam, officiis expedita voluptates voluptatibus tempora quibusdam, minima modi sit reprehenderit distinctio? Beatae nam quisquam architecto tempora at odit magnam, nulla sed consequatur dicta, ut laudantium ipsum ex, molestiae ratione quas doloremque quibusdam eveniet.
+          Sint ex necessitatibus excepturi.
+          Ipsum, molestias rerum. Nobis esse possimus, labore, adipisci itaque aspernatur quisquam sit, incidunt explicabo tenetur omnis.
+          Impedit quasi repellendus doloribus? Ullam ab maxime sed?</p>
+      </div>
     </div>
   )
 }
