304fc6d524bb263e1ce24f3617523358478adf0e
abhinav khanna <kabhinav99@gmail.com>
2024-12-09 23:54:54 +0530

    feat: thankyou and page-not-found

---
+++ src/app/page-not-found/page.jsx
@@ -0,0 +1,36 @@
+import Header from '@/components/ui/header'
+import SubHeader from '@/components/ui/sub-header'
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+  return (
+    <>
+      <SubHeader />
+      <Header />
+      <div className="flex flex-col justify-center items-center h-screen overflow-hidden">
+        <Image
+          src="/Frame 104.svg"
+          width={100}
+          height={100}
+          alt="Deal Icon"
+        />
+        <div className="text-secondary text-4xl py-2 heading-font">
+          PAGE NOT FOUND
+        </div>
+        <div className="text-center flex flex-col items-center justify-center px-4">
+          <p>Oops! The server was able to communicate with the browser, but it could'nt find the requested page.</p>
+          <p>This can happen when a page is moved , deleted, or he URL is mistyped.</p>
+        </div>
+        <div className="pt-6  ">
+          <Button className="text-white text-lg  bg-secondary hover:bg-[#B0CB1F] hover:text-secondary font-bold p-4 ">
+            Back to Home
+          </Button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/thank-you/page.jsx
@@ -0,0 +1,36 @@
+import Header from '@/components/ui/header'
+import SubHeader from '@/components/ui/sub-header'
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+  return (
+    <>
+      <SubHeader />
+      <Header />
+      <div className="flex flex-col justify-center items-center h-screen overflow-hidden">
+        <Image
+          src="/deal 1.svg"
+          width={100}
+          height={100}
+          alt="Deal Icon"
+        />
+        <div className="text-secondary text-4xl py-2 heading-font">
+          THANK YOU
+        </div>
+        <div className="text-center flex flex-col items-center justify-center px-4">
+          <p>You're one step closer to a smooth and successful exhibition!</p>
+          <p>Your request has been successfully submitted. Our team will connect with you shortly.</p>
+        </div>
+        <div className="mt-6 ">
+          <Button className="text-white text-lg bg-secondary hover:bg-[#B0CB1F] hover:text-secondary font-bold p-4 ">
+            Back to Home
+          </Button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default page
