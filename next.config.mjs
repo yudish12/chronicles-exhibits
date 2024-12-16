c3ea708e83ab4b417ba3426311a679fb3f3f69fe
yudish12 <yudish.chakrawarty@mindcase.co>
2024-12-16 23:45:19 +0530

    booth size rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //https://stackoverflow.com/questions/61880435/adding-prefix-to-nextjs-dynamic-route for rewrites
+  rewrites: async () => {
+    return [
+      {
+        source: "/:size-trade-show-booth",
+        destination: "/booth/size/:size",
+      },
+    ];
+  },
   images: {
-    domains: ['utfs.io'], // Add the domain here
+    domains: ["utfs.io"], // Add the domain here
   },
 };
 
