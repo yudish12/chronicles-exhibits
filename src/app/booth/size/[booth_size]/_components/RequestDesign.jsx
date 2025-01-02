f41dd0623d88651cb4ce80e7b2d242f2e27d328a
yudish12 <yudish.chakrawarty@mindcase.co>
2025-01-02 13:06:10 +0530

    size fix

--- src/app/booth/size/[booth_size]/_components/RequestDesign.jsx
+++ src/app/booth/size/[booth_size]/_components/RequestDesign.jsx
@@ -5,14 +5,14 @@ const RequestDesign = ({ fields }) => {
   return (
     <div className="w-full product-bg flex flex-col items-center justify-center py-14 px-4 text-center">
       <div className="font-bold uppercase text-lg md:text-xl pt-10 text-[#B0CB1F]">
-        {fields[5].value}
+        {fields[4].value}
       </div>
       <div className="text-2xl sm:text-3xl uppercase md:text-4xl lg:text-4xl heading-font text-white font-bold pt-10">
-        {fields[6].value}
+        {fields[5].value}
       </div>
       <div className="py-10">
-        <Button className="bg-transparent text-wrap h-16 sm:h-auto text-base sm:text-lg border-2 border-[#B0CB1F] hover:bg-[#B0CB1F] px-6 sm:px-8 py-4 sm:py-6 font-bold text-[#B0CB1F] hover:text-white">
-          {fields[7].value}
+        <Button className="bg-transparent text-wrap h-16 sm:h-auto text-base sm:text-lg border-2 border-[#B0CB1F] hover:bg-[#B0CB1F] px-6 sm:px-8 font-bold text-[#B0CB1F] hover:text-white">
+          {fields[6].value}
         </Button>
       </div>
     </div>
