5829bedd3d539ec221aae72da11e3cfa24e8d6df
yudish12 <yudish.chakrawarty@mindcase.co>
2025-01-29 10:05:46 +0530

    fixing api

--- src/app/api/page/all-locations/route.js
+++ src/app/api/page/all-locations/route.js
@@ -1,11 +1,11 @@
 import dbConnect from '@/config/db-connect';
-import Pages from "../models/pages";
+import Pages from "@/server/models/pages";
 import { getActionFailureResponse, getActionSuccessResponse } from '@/utils';
 
 export async function GET(){
     try {
         await dbConnect();
-        const pages = await Pages.find({name:"locations"}).lean();
+        const pages = await Pages.findOne({name:"locations"}).lean();
         return Response.json(getActionSuccessResponse(pages));
     } catch (error) {
         return Response.json(getActionFailureResponse(error,"toast"), { status: 500 });
