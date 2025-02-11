b81ec16f74b9e800feb3e6869f4b4331745adfef
abhinav khanna <kabhinav99@gmail.com>
2025-02-11 21:57:25 +0530

    feat:optimizing hero section gif

--- src/app/(landing)/Hero.jsx
+++ src/app/(landing)/Hero.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import React from "react";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 const EnquiryForm = dynamic(() => import("@/components/Form"))
 
 const Hero = ({ fields }) => {
@@ -12,8 +13,23 @@ const Hero = ({ fields }) => {
         <h1 className="heading-font py-8 sm:py-10 sm:w-full xs:w-[90%] w-[85%] mx-auto uppercase text-center text-black font-medium text-4xl sm:text-[2.75rem]">
           {fields[0].value}
         </h1>
-        <div className="block hero sm:h-1/2 h-full lg:w-2/3 w-4/5 mx-auto shadow-xl rounded-[32px] relative"></div>
+        {/* <Image className="block hero sm:h-1/2 h-full lg:w-2/3 w-4/5 mx-auto shadow-xl rounded-[32px] relative"
+        priority={true}
+         /> */}
 
+    <div className="relative overflow-hidden bg-black/70">
+      <Image
+        src="/hero-bg.gif" // Ensure this is in the public folder
+        alt="Hero Background"
+        layout="fill"
+        objectFit="cover"
+        priority={true} // Optimizes LCP by preloading
+        loading="eager"
+        decoding="async" 
+        fetchPriority="high"
+        className="absolute top-0 left-0 w-full h-full mix-blend-darken opacity-80 -z-10" 
+      />
+    </div>
         {/* Absolute Text and Button Container */}
         <div className="left-[20%] px-6 sm:px-12 md:px-20 lg:p-8 p-4 rounded-[32px] justify-center lg:absolute flex flex-col gap-8 lg:w-[60%] w-full bg-white top-[43%] 2xl:top-[47%] lg:mt-16 sm:mt-4 mt-0">
           <h3 className="text-2xl sm:text-3xl text-wrap 2xl:mt-2 break-words text-center text-primary uppercase font-medium">
